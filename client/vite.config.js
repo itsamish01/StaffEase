522f3d77a0a8733b1e49949e839c022d15ece6f8
Joshua <janorris65@gmail.com>
2023-03-28 23:18:04 -0500

    setup template client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +7,16 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": "/src",
+      "@components": "/src/components",
+      "@hooks": "/src/hooks",
+      "@routes": "/src/routes",
+      "@services": "/src/services",
+      "@utils": "/src/utils",
     },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
   },
 });
