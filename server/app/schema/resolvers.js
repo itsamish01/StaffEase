ccbf1d2ac9a12e75952c59ac3569de93e147f280
Jason Charney <jrcharney@gmail.com>
2023-03-31 18:55:58 -0500

    Renamed some stuff. Make sure clockInEmployee has a capital I

--- server/app/schema/resolvers.js
+++ server/app/schema/resolvers.js
@@ -28,45 +28,77 @@ const resolvers = {
   },
 
   Mutation: {
-    // Sign up a new Business user
+    /**
+     * @func addBusiness
+     * @desc Sign up (create) a new Business User
+     * @todo Rename this to "createBusiness"
+     * @param {*} _
+     * @param {*} args
+     * @returns {object} JWT token?
+     */
     addBusiness: async (_, args) => {
+      console.log("addBusiness");
       const business = await Business.create(args);
       const token = await business.authenticate(args.password);
       return { token };
     },
-    // Log in an existing Business user
+    /**
+     * @func loginBusiness
+     * @desc Login an existing Business user
+     * @param {*} _
+     * @param {object} param1
+     * @returns {object}  JWT Token? and a business
+     */
     loginBusiness: async (_, { email, password }) => {
-      console.log("hello");
+      console.log("loginBusiness");
       const business = await Business.findOne({ email });
-
       if (!business) {
         throw new Error("Incorrect credentials");
       }
       const token = business?.authenticate(password);
-
       return { token, business };
     },
-    // Sign up a new Employee user
+    /**
+     * @func addEmployee
+     * @desc Sign up (create) a new Employee user.
+     * @todo rename this to "createEmployee"
+     * @param {*} _
+     * @param {*} args
+     * @returns {object} JWT Token?
+     */
     addEmployee: async (_, args) => {
+      console.log("addEmployee");
       const employee = await Employee.create(args);
       const token = await employee.authenticate(args.password);
       return { token };
     },
-    // Log in an existing Employee user
+    /**
+     * @func loginEmployee
+     * @desc Log in an existing Employee user
+     * @param {*} _
+     * @param {object} param1
+     * @returns {object} JWT Token? and an employee
+     */
     loginEmployee: async (_, { email, password }) => {
-      console.log("hello");
+      console.log("loginEmployee");
       const employee = await Employee.findOne({ email });
-
       if (!employee) {
         throw new Error("Incorrect credentials");
       }
       const token = employee?.authenticate(password);
-
       return { token, employee };
     },
-    // Update an Employee
+    /**
+     * @func saveEmployee
+     * @desc update a Business by adding an Employee to the list of employees.
+     * @todo rename this to "addEmployee"
+     * @param {*} _
+     * @param {object} param1
+     * @param {*} context
+     * @returns {*} updatedBusiness
+     */
     saveEmployee: async (_, { firstName, lastName }, context) => {
-      console.log("helloEmployee");
+      console.log("saveEmployee");
       if (context.user) {
         const employeeData = await Employee.findOne({ firstName, lastName });
         const updatedBusiness = await Business.findByIdAndUpdate(
@@ -74,34 +106,45 @@ const resolvers = {
           { $push: { employees: employeeData } },
           { new: true }
         );
-
         return updatedBusiness;
       }
-
       throw new Error("You need to be logged in!");
     },
-    // Delete an Employee
+    /**
+     * @func removeEmployee
+     * @desc update a Business by removing an Employee from the list of employees.
+     * @param {*} _
+     * @param {object} param1
+     * @param {*} context
+     * @returns updatedBusiness
+     */
     removeEmployee: async (_, { employeeId }, context) => {
       if (context.user) {
-        //console.log(employeeId);
+        console.log("removeEmployee");
         const updatedBusiness = await Business.updateOne(
           { _id: context.user.id },
           { $pull: { employees: { _id: employeeId } } }
         );
-        // console.log(employeeId);
-        //console.log(context.user.id);
         return updatedBusiness;
       }
-
       throw new Error("You need to be logged in!");
     },
-    // Add a new Review (note: formerly saveCustomer)
+    /**
+     * @func saveReview
+     * @desc Save (create) a Review of a Business.
+     * @note This method was formerly called saveCustomer
+     * @todo rename this again to "addReview" or "createReview"
+     * @param {*} _
+     * @param {*} param1
+     * @param {*} context
+     * @returns updatedBusiness
+     */
     saveReview: async (
       _,
       { firstName, lastName, rating, comment },
       context
     ) => {
-      console.log("Hello Customer");
+      console.log("saveReview");
       if (context.user) {
         const updatedBusiness = await Business.findByIdAndUpdate(
           { _id: context.user.id },
@@ -113,30 +156,46 @@ const resolvers = {
 
       throw new Error("You need to be logged in!");
     },
-    // Clock in an Employee
-    // TODO: Isn't there supposed to be something to clock out the employee?
-    clockinEmployee: async (_, { businessName }, context) => {
-      console.log("helloEmployeeClock");
+    /**
+     * @func clockInEmployee
+     * @desc Clock In/Out an Employee
+     * @todo Shouldn't there be two methods to clock in and clock out an employee? Also timestamps!
+     * @param {*} _
+     * @param {*} param1
+     * @param {*} context
+     * @returns updatedEmployee
+     */
+    clockInEmployee: async (_, { businessName }, context) => {
+      console.log("clockInEmployee");
       if (context.user) {
-        // TODO: Use some better variable names
-        const employeePreB = await Employee.findById({ _id: context.user.id });
-        let clockboolean = employeePreB.clockedin;
-        clockboolean = !clockboolean;
+        // TODO: If I reuse employee, consider using let instead of const
+        const employee = await Employee.findById({ _id: context.user.id });
+        let clockBoolean = employee.clockedIn;
+        clockBoolean = !clockBoolean;
 
-        const assocBusiness = await Business.findOne({ businessName });
-        assocBusiness.employees.id(context.user.id).clockedin = clockboolean;
-        await assocBusiness.save();
+        const business = await Business.findOne({ businessName });
+        business.employees.id(context.user.id).clockedin = clockBoolean;
+        await business.save();
+        // TODO: Can I just reuse employee
+        // TODO: timestamps
         const updatedEmployee = await Employee.findByIdAndUpdate(
           { _id: context.user.id },
-          { $set: { clockedin: clockboolean } }
+          { $set: { clockedIn: clockBoolean } }
         );
-        return assocBusiness;
+        // We should be returning the employee not the business here.
+        // return assocBusiness;
+        return updatedEmployee;
       }
 
       throw new Error("You need to be logged in!");
     },
     // TODO: write virtual
     // TODO: write a update current capacity
+    /*
+    currentCapacity: async (_, { businessName }. context) => {
+      throw new Error("You need to be logged in!");
+    }
+    */
   },
 };
 
