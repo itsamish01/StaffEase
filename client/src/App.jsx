e910b35b4ba0530507d7151954beef94dc982a4f
AyeMishDior <itsamish02@gmail.com>
2023-04-03 00:58:25 -0500

    messy commit, updated files to jx and reconfigured files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import EmployeeLogin from './components/EmployeeLogin';
+import EmployeeDashboard from './components/EmployeeDashboard';
+import RestaurantSearch from './components/RestaurantSearch';
+import ReviewForm from './components/ReviewForm';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import TestComponent from './components/TestComponent'; // Add this import
 
 function App() {
   return (
-    <div>
-      <h1 className='text-2xl text-red-500'>Welcome to StaffEase</h1>
-      <p>This is a React application.</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={EmployeeLogin} />
+          <Route path="/dashboard" component={EmployeeDashboard} />
+          <Route path="/search" component={TestComponent} /> {/* Change this line */}
+          <Route path="/submit-review" component={ReviewForm} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +13,7 @@ export default defineConfig({
       "@services": "/src/services",
       "@utils": "/src/utils",
     },
+    extensions: ['.mjs', '.js', '.jsx', '.json'], // Add this line
   },
   test: {
     globals: true,
